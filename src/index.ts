4d14e5b6290537fdd9220d1c217ba889b88f965e
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-03 01:35:13 +0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Basic Types.
+var isDone: boolean = true;
+var height: number = 6;
+var myname: string = 'test';
+var list: number[] = [1, 2, 3]; // Also: `var list: Array<number> = [1, 2, 3]` which is using generics.
+
+// Enums.
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+var c: Color = Color.Green;
