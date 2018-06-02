bc497a828e9297bfb26beab36a33083906eb6be5
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-03 01:54:46 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var notify = require('gulp-notify');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('default', function() {
+gulp.task('tsc', function() {
   return tsProject
     .src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+    .js.pipe(gulp.dest('dist'))
+    .pipe(notify('Scripts Compiled'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.ts', ['tsc']);
+});
+
+gulp.task('default', ['tsc', 'watch']);
