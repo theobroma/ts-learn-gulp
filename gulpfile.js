0445ae3f20d60daff9c15b2b515529e25900e635
Alexandr Siryj <theobroma222@gmail.com>
2020-01-10 12:12:32 +0200

    gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
-var gulp = require('gulp');
-var ts = require('gulp-typescript');
-var notify = require('gulp-notify');
-var tsProject = ts.createProject('tsconfig.json');
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var notify = require("gulp-notify");
+var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('tsc', function() {
+gulp.task("tsc", function() {
   return tsProject
     .src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest('dist'));
+    .js.pipe(gulp.dest("dist"))
+    .pipe(notify("Scripts Compiled"));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.ts', ['tsc']);
+gulp.task("watch", function() {
+  gulp.watch("src/*.ts", gulp.series("tsc"));
 });
 
-gulp.task('default', ['tsc', 'watch']);
+gulp.task("default", gulp.series("tsc", "watch"));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 enum TagActions {
-  Search = 'TAGS_SEARCH',
-  SearchUpdate = 'TAGS_SEARCH_UPDATE',
-  SearchError = 'TAGS_SEARCH_ERROR'
+  Search = "TAGS_SEARCH",
+  SearchUpdate = "TAGS_SEARCH_UPDATE",
+  SearchError = "TAGS_SEARCH_ERROR"
 }
