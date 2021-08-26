91b5f4ba928eaefce2d65d0a9be7f3fbede46389
Alexandr <theobroma222@gmail.com>
2021-08-26 19:10:16 +0300

    custom record

---
+++ src/reacord.ts
@@ -0,0 +1,24 @@
+type MyRecord<K extends string | number | symbol, T> = { [P in K]: T; }
+
+interface CatInfo {
+    age: number;
+    breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+    miffy: { age: 10, breed: "Persian" },
+    boris: { age: 5, breed: "Maine Coon" },
+    mordred: { age: 16, breed: "British Shorthair" },
+};
+
+const mycats: MyRecord<CatName, CatInfo> = {
+    miffy: { age: 10, breed: "Persian" },
+    boris: { age: 5, breed: "Maine Coon" },
+    mordred: { age: 16, breed: "British Shorthair" },
+};
+
+
+cats.boris;
+mycats.boris;
