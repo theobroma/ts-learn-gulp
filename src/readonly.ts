31adf9326e0dd66701e3bd910419273bee0895db
Alexandr <theobroma222@gmail.com>
2021-08-26 18:55:06 +0300

    custom readonly

---
+++ src/readonly.ts
@@ -0,0 +1,18 @@
+// type MyPartial<T> = { [P in keyof T]?: T[P] | undefined; };
+type MyReadonly<T> = { readonly [P in keyof T]: T[P]; }
+
+interface Todo2 {
+    title: string;
+}
+
+const todo: Readonly<Todo2> = {
+    title: "Delete inactive users",
+};
+
+const mytodo: MyReadonly<Todo2> = {
+    title: "Delete inactive users",
+};
+
+
+todo.title = "Hello";
+mytodo.title = "Hello";
