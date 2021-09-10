77a42d9e1ce589285bb8f4b3b528777e35ad1636
Alexandr Siryj <theobroma222@gmail.com>
2021-09-10 23:00:23 +0300

    omit

---
+++ src/UtilityTypes/omit.ts
@@ -0,0 +1,28 @@
+interface Todo3 {
+    title: string;
+    description: string;
+    completed: boolean;
+    createdAt: number;
+}
+
+type TodoPreview3 = Omit<Todo3, "description">;
+
+const todo3: TodoPreview3 = {
+    title: "Clean room",
+    completed: false,
+    createdAt: 1615544252770,
+    // Uncomment to see error
+    // description:"12313"
+  };
+
+type TodoInfo3 = Omit<Todo3, "completed" | "createdAt">;
+
+const todoInfo3: TodoInfo3 = {
+  title: "Pick up kids",
+  description: "Kindergarten closes at 5pm",
+  // Uncomment to see error
+  // completed: false,
+  // createdAt: 999,
+};
+
+// TODO implement MyOmit
