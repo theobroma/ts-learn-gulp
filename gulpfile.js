f4514943b4072485ce1367479e7fae3833fe306f
Alexandr Siryj <theobroma222@gmail.com>
2021-09-10 23:27:17 +0300

    add del lib

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
+var del = require('del');
 var gulp = require("gulp");
-var ts = require("gulp-typescript");
 var notify = require("gulp-notify");
+var ts = require("gulp-typescript");
 var tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("tsc", function() {
@@ -11,8 +12,13 @@ gulp.task("tsc", function() {
     // .pipe(notify("Scripts Compiled"));
 });
 
+gulp.task('clean', function() {
+    return del('dist/**', { force: true });
+});
+
 gulp.task("watch", function() {
     gulp.watch("src/*.ts", gulp.series("tsc"));
 });
 
-gulp.task("default", gulp.series("tsc", "watch"));
+// gulp.task("default", gulp.series("clean"));
+gulp.task("default", gulp.series("clean", "tsc", "watch"));
