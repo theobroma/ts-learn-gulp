c20154fdc807fa0651d5dcd663a796858a20ecb5
Alexandr Siryj <theobroma222@gmail.com>
2021-09-11 03:42:36 +0300

    instance

---
+++ src/UtilityTypes/instance.ts
@@ -0,0 +1,24 @@
+class CCC {
+    x = 0;
+    y = 0;
+  }
+
+type TC = InstanceType<typeof CCC>;
+
+
+
+// ===================================----=======================================
+// https://scriptdev.ru/ts/045#instancetype
+
+class Animal {
+    move(): void {}
+}
+
+type Class = typeof Animal;
+
+
+function factory(Class: typeof Animal) {
+    type Instance = InstanceType<Class>;
+
+    let instance: Instance = new Class(); // Ok -> let instance: Animal
+}
