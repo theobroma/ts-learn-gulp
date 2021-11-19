e2dc5d620318e5c38f3d8c828b81e379224cdf12
Alexandr Siryj <theobroma222@gmail.com>
2021-11-19 10:44:42 +0200

    boolean

---
+++ src/AdvancedTypeScript/boolean.ts
@@ -0,0 +1,6 @@
+// https://fettblog.eu/symbols-in-javascript-and-typescript/
+
+const boolTrue: boolean = true; // 👍
+const boolFalse: boolean = false; // 👍
+// const boolUndefined: boolean = undefined; // 💥
+// const boolNull: boolean = null; // 💥
