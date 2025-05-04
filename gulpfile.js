161c65837091a7c096dd07b600d6a7467294bcdf
Alexandr Siryj <theobroma222@gmail.com>
2025-05-04 04:09:31 +0300

    upd libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
 var del = require('del');
 var gulp = require('gulp');
-var notify = require('gulp-notify');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('tsc', function() {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
-    // .pipe(notify("Scripts Compiled"));
+gulp.task('tsc', function () {
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function() {
-    return del('dist/**', { force: true });
+gulp.task('clean', function () {
+  //   return del('dist/**', { force: true }); old version6
+  return del.deleteAsync(['dist/**', '!dist']);
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/*.ts', gulp.series('tsc'));
+gulp.task('watch', function () {
+  gulp.watch('src/*.ts', gulp.series('tsc'));
 });
 
-// gulp.task("default", gulp.series("clean"));
+// gulp.task('default', gulp.series('clean'));
 gulp.task('default', gulp.series('clean', 'tsc', 'watch'));
